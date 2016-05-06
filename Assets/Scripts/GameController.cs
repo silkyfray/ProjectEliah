ae503ecc81a30351339801e0eac6b3c8072dcf3b
Antonio Nikolov <anikolovn@gmail.com>
2016-05-06 19:29:55 +0100

    Second Commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    //Prefabs
+    public GameObject mColorObjectPrefab;
+
+    private RuntimePreferences mRuntimePreferences;
+
+    // Use this for initialization
+    void Start()
+    {
+        mRuntimePreferences = GetComponent<RuntimePreferences>();
+        SpawnTestBalls();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnTestBalls()
+    {
+        
+    }
+}
+
---
+++ Assets/Scripts/RuntimePreferences.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuntimePreferences : MonoBehaviour
+{
+    public int mInitialBalls = 10;
+    public float mInitialGraceTime = 1.0f;
+    public float mBallSpeed = 1.0f;
+}
